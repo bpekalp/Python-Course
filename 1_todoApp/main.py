7d2f47126f46eab3e6075bfcf2819088f5676652
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-10-08 15:26:07 +0300

    While loops

--- 1_todoApp/main.py
+++ 1_todoApp/main.py
@@ -1,15 +1,5 @@
 prompt = "Enter To-Do: "
 
-todo1 = input(prompt)
-todo2 = input(prompt)
-todo3 = input(prompt)
-
-todos = [todo1, todo2, todo3]
-
-print(todos)
-
-todoType = type(todo1)
-todosType = type(todos)
-
-print(todoType)
-print(todosType)
+while True:
+    todo = input(prompt)
+    print(todo)
