5e86b19c21fb259d13ba0f6ebad59c47cf43cb85
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-10-08 16:12:17 +0300

    Match case implementation

--- 1_todoApp/main.py
+++ 1_todoApp/main.py
@@ -1,9 +1,22 @@
-prompt = "Enter To-Do: "
+optionPrompt = "Type add, show or exit: "
+option = ""
+
+todoPrompt = "Enter a To-Do: "
 todo = ""
+
 todos = []
 
 while True:
-    todo = input(prompt)
-    todo = todo.title()
-    todos.append(todo)
-    print(todos)
+    option = input(optionPrompt)
+    option = option.lower()
+    match option:
+        case "add":
+            todo = input(todoPrompt)
+            todo = todo.capitalize()
+            todos.append(todo)
+        case "show":
+            print(todos)
+        case "exit":
+            break
+        case _:
+            print("Wrong input, try again.")
