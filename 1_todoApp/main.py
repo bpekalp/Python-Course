30f66e81b83566c268c42dc99126dbec975e19cb
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-10-09 16:33:14 +0300

    Read write operations on .txt files
    Storing the To-Do list on a text file. Implemented for "add" and "show" match cases.

--- 1_todoApp/main.py
+++ 1_todoApp/main.py
@@ -1,37 +1,52 @@
-todo = ""
-todoPrompt = "Enter a To-Do: "
-
 todos = []
 
-option = ""
+todoPrompt = "Enter a To-Do: "
 optionPrompt = "Type add, edit, show, complete or exit: "
-
-num = 0
 numPrompt = "Got it. Which one? "
 
-row = ""
+filePath = "./data/.todos.txt"
+file = open(filePath, "r")
+file.close()
 
 while True:
     option = input(optionPrompt).lower().strip()
+
     match option:
         case "add":
-            todo = input(todoPrompt).capitalize().strip()
+            file = open(filePath, "r")
+            todos = file.readlines()
+            file.close()
+
+            todo = input(todoPrompt).capitalize().strip() + "\n"
             todos.append(todo)
+
+            file = open(filePath, "w")
+            file.writelines(todos)
+            file.close()
+
         case "edit":
             num = int(input(numPrompt)) - 1
             todo = input(todoPrompt).capitalize().strip()
             todos[num] = todo
+
         case "show":
+            file = open(filePath, "r")
+            todos = file.readlines()
+            file.close()
+
             for i, item in enumerate(todos, start=1):
                 row = f"{i}. {item}"
                 print(row)
+
         case "complete":
             num = int(input(numPrompt)) - 1
             todo = todos.pop(num)
             row = f"{todo} is marked as done and removed from the list."
             print(row)
+
         case "exit":
             break
+
         case _:
             row = "Wrong input, try again."
             print(row)
