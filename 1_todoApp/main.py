4355327d290c07df18b4ab4faf46660c5a891fd0
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-10-10 15:39:49 +0300

    Code improvements
    Implement "With Context Manager" and define functions to get rid of repeating codes

--- 1_todoApp/main.py
+++ 1_todoApp/main.py
@@ -6,44 +6,81 @@ numPrompt = "Got it. Which one? "
 
 filePath = "data/.todos.txt"
 
-while True:
+
+def getOptionFromUser(optionPrompt):
     option = input(optionPrompt).lower().strip()
+    return option
+
+
+def readTodos(filePath):
+    with open(filePath, "r") as file:
+        todos = file.readlines()
+    return todos
+
+
+def writeTodos(todos, filePath):
+    with open(filePath, "w") as file:
+        file.writelines(todos)
+
+
+def getTodoFromUser(todoPrompt):
+    todo = input(todoPrompt).capitalize().strip() + "\n"
+    return todo
+
+
+def getIndexFromUser(numPrompt):
+    num = int(input(numPrompt)) - 1
+    return num
+
+
+def displayTodos(todos):
+    # todos = [todo.strip("\n") for todo in todos]
+
+    for i, todo in enumerate(todos, start=1):
+        todo = todo.strip("\n")
+        row = f"{i}. {todo}"
+        print(row)
+
+
+def popTodo(todos, num):
+    todo = todos.pop(num).capitalize().strip("\n")
+    return todo
+
+
+while True:
+    option = getOptionFromUser(optionPrompt)
 
     match option:
         case "add":
-            file = open(filePath, "r")
-            todos = file.readlines()
-            file.close()
+            todos = readTodos(filePath)
 
-            todo = input(todoPrompt).capitalize().strip() + "\n"
+            todo = getTodoFromUser(todoPrompt)
             todos.append(todo)
 
-            file = open(filePath, "w")
-            file.writelines(todos)
-            file.close()
+            writeTodos(todos, filePath)
 
         case "edit":
-            num = int(input(numPrompt)) - 1
-            todo = input(todoPrompt).capitalize().strip()
+            todos = readTodos(filePath)
+
+            num = getIndexFromUser(numPrompt)
+            todo = getTodoFromUser(todoPrompt)
             todos[num] = todo
 
+            writeTodos(todos, filePath)
+
         case "show":
-            file = open(filePath, "r")
-            todos = file.readlines()
-            file.close()
-
-            # todos = [todo.strip("\n") for todo in todos]
-
-            for i, todo in enumerate(todos, start=1):
-                todo = todo.strip("\n")
-                row = f"{i}. {todo}"
-                print(row)
+            todos = readTodos(filePath)
+            displayTodos(todos)
 
         case "complete":
-            num = int(input(numPrompt)) - 1
-            todo = todos.pop(num)
+            todos = readTodos(filePath)
+
+            num = getIndexFromUser(numPrompt)
+            todo = popTodo(todos, num)
             row = f"{todo} is marked as done and removed from the list."
             print(row)
+
+            writeTodos(todos, filePath)
 
         case "exit":
             break
