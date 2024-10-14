7920e99fa5779a10de4fe1980f7e7a78327d45f4
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-10-14 12:16:43 +0300

    Improve "edit" and "complete" cases

--- 1_todoApp/main.py
+++ 1_todoApp/main.py
@@ -20,30 +20,45 @@ while True:
             file.writelines(todos)
 
     elif "edit" in option:
-        num = int(input(numPrompt)) - 1
-        todo = input(todoPrompt).capitalize().strip()
+        with open(filePath, "r") as file:
+            todos = file.readlines()
+
+        numStr = option.lstrip("edit").strip()
+        num = int(numStr) - 1
+
+        todo = input(todoPrompt).capitalize().strip() + "\n"
         todos[num] = todo
+
+        with open(filePath, "w") as file:
+            file.writelines(todos)
 
     elif "show" in option:
         with open(filePath, "r") as file:
             todos = file.readlines()
 
         # todos = [todo.strip("\n") for todo in todos]
-
         for i, todo in enumerate(todos, start=1):
             todo = todo.strip("\n")
-            row = f"{i}. {todo}"
-            print(row)
+            message = f"{i}. {todo}"
+            print(message)
 
     elif "complete" in option:
-        num = int(input(numPrompt)) - 1
-        todo = todos.pop(num)
-        row = f"{todo} is marked as done and removed from the list."
-        print(row)
+        with open(filePath, "r") as file:
+            todos = file.readlines()
+
+        numStr = option.lstrip("complete").strip()
+        num = int(numStr) - 1
+
+        todo = todos.pop(num).strip("\n")
+        message = f"{todo} is marked as done and removed from the list."
+        print(message)
+
+        with open(filePath, "w") as file:
+            file.writelines(todos)
 
     elif "exit" in option:
         break
 
     else:
-        row = "Wrong input, try again."
-        print(row)
+        message = "Wrong input, try again."
+        print(message)
