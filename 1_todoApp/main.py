b566a50704f4e50e6b7a0043ec7b00e23b284151
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-10-14 16:54:51 +0300

    if __name__ == "__main__"

--- 1_todoApp/main.py
+++ 1_todoApp/main.py
@@ -24,77 +24,82 @@ def numToString(option, userIf):
     return num
 
 
-while True:
-    option = input(optionPrompt).lower().strip()
+def main():
+    while True:
+        option = input(optionPrompt).lower().strip()
 
-    if option.startswith("add"):
-        todos = readTodos(filePath)
-
-        userIf = "add"
-        todo = option.lstrip(userIf).strip().capitalize() + "\n"
-        todos.append(todo)
-
-        writeTodos(todos, filePath)
-
-    elif option.startswith("edit"):
-        try:
+        if option.startswith("add"):
             todos = readTodos(filePath)
 
-            userIf = "edit"
-            num = numToString(option, userIf)
-
-            todo = input(todoPrompt).strip().capitalize() + "\n"
-            todos[num] = todo
+            userIf = "add"
+            todo = option.lstrip(userIf).strip().capitalize() + "\n"
+            todos.append(todo)
 
             writeTodos(todos, filePath)
 
-        except ValueError:
-            message = "Usage: edit <To-Do number to edit>"
-            print(message)
-            continue
+        elif option.startswith("edit"):
+            try:
+                todos = readTodos(filePath)
 
-        except IndexError:
-            message = "The number was out of bounds of the To-Do list."
-            print(message)
-            continue
+                userIf = "edit"
+                num = numToString(option, userIf)
 
-    elif option.startswith("show"):
-        todos = readTodos(filePath)
+                todo = input(todoPrompt).strip().capitalize() + "\n"
+                todos[num] = todo
 
-        # todos = [todo.strip("\n") for todo in todos]
-        for i, todo in enumerate(todos, start=1):
-            todo = todo.strip("\n")
-            message = f"{i}. {todo}"
-            print(message)
+                writeTodos(todos, filePath)
 
-    elif option.startswith("complete"):
-        try:
+            except ValueError:
+                message = "Usage: edit <To-Do number to edit>"
+                print(message)
+                continue
+
+            except IndexError:
+                message = "The number was out of bounds of the To-Do list."
+                print(message)
+                continue
+
+        elif option.startswith("show"):
             todos = readTodos(filePath)
 
-            userIf = "complete"
-            num = numToString(option, userIf)
+            # todos = [todo.strip("\n") for todo in todos]
+            for i, todo in enumerate(todos, start=1):
+                todo = todo.strip("\n")
+                message = f"{i}. {todo}"
+                print(message)
 
-            todo = todos.pop(num).strip("\n")
-            message = f"{todo} is marked as done and removed from the list."
+        elif option.startswith("complete"):
+            try:
+                todos = readTodos(filePath)
+
+                userIf = "complete"
+                num = numToString(option, userIf)
+
+                todo = todos.pop(num).strip("\n")
+                message = f"{todo} is marked as done and removed from the list."
+                print(message)
+
+                writeTodos(todos, filePath)
+
+            except ValueError:
+                message = "Usage: complete <To-Do number to complete>"
+                print(message)
+                continue
+
+            except IndexError:
+                message = "The number was out of bounds of the To-Do list."
+                print(message)
+                continue
+
+        elif option.startswith("exit"):
+            message = "Goodbye!"
+            print(message)
+            break
+
+        else:
+            message = "Wrong input, try again."
             print(message)
 
-            writeTodos(todos, filePath)
 
-        except ValueError:
-            message = "Usage: complete <To-Do number to complete>"
-            print(message)
-            continue
-
-        except IndexError:
-            message = "The number was out of bounds of the To-Do list."
-            print(message)
-            continue
-
-    elif option.startswith("exit"):
-        message = "Goodbye!"
-        print(message)
-        break
-
-    else:
-        message = "Wrong input, try again."
-        print(message)
+if __name__ == "__main__":
+    main()
