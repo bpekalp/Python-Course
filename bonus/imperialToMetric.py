8b88ac29c205cab053b82c29e5a8075182975654
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-10-15 16:19:54 +0300

    Function decoupling for length converter app

--- bonus/imperialToMetric.py
+++ bonus/imperialToMetric.py
@@ -1,6 +1,10 @@
-def convertToCm(length):
+def parsing(length):
     length = length.split("'")
     feet, inches = float(length[0]), float(length[1])
+    return feet, inches
+
+
+def convertToCm(feet, inches):
 
     cm = feet * 30.48 + inches * 2.54
     return cm
@@ -14,9 +18,10 @@ while True:
             try:
                 length = input("Enter feet and inches e.g 5'7: ")
 
-                cm = convertToCm(length)
+                feet, inches = parsing(length)
+                cm = convertToCm(feet, inches)
 
-                print(f"{length} is {cm:.2f}cm.")
+                print(f"{feet:.0f}'{inches:.0f} is {cm:.2f}cm.")
             except ValueError:
                 print("Wrong input. Try again.")
 
