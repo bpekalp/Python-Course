380e17d4109cf9d10378132b1b43dffd18b99ce2
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-10-15 13:35:20 +0300

    Feet and inches to centimeters converter

---
+++ bonus/imperialToMetric.py
@@ -0,0 +1,27 @@
+def convertToCm(length):
+    length = length.split("'")
+    feet, inches = float(length[0]), float(length[1])
+    feet *= 30.48
+    inches *= 2.54
+
+    cm = feet + inches
+    return cm
+
+
+while True:
+    option = input("Type enter or exit: ")
+
+    match option:
+        case "enter":
+            try:
+                length = input("Enter feet and inches e.g 5'7: ")
+
+                cm = convertToCm(length)
+
+                print(f"{length} is {cm:.2f}cm.")
+            except ValueError:
+                print("Wrong input. Try again.")
+
+        case "exit":
+            print("Goodbye!")
+            break
