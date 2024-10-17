f65aa399e011491a46f034250b3c0f2548b71441
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-10-17 16:45:22 +0300

    Implement "edit" feature

---
+++ 1_todoApp/func/textFormatter.py
@@ -0,0 +1,12 @@
+def textBoxToTodo(_value, _key):
+    """Reads text box and formats the content before writing into todos file
+
+    Args:
+        _value (dict): The dictionary item provided by FreeSimpleGUI library.
+        _key (str): The key of item.
+
+    Returns:
+        str: Returns the formatted To-Do item.
+    """
+    todo = str(_value[_key]).strip().capitalize() + "\n"
+    return todo
--- 1_todoApp/gui.py
+++ 1_todoApp/gui.py
@@ -1,4 +1,5 @@
 import func.readWrite as rw
+import func.textFormatter as tf
 import func.stringToNum as stn
 import func.dateTimeFormatter as dtf
 import FreeSimpleGUI as sg
@@ -11,34 +12,63 @@ title = "Coolest To-Do App Ever!"
 
 today = sg.Text(key="kToday", text=dtf.formatDateTime())
 
-labelToDoText = sg.Text(key="kLabelToDo", text="Enter a To-Do")
-toDoText = sg.InputText(key="kToDo", tooltip="Here goes your To-Do!")
+labelTodoText = sg.Text(key="kLabelTodo", text="Enter a To-Do")
+todoText = sg.InputText(key="kTodo", tooltip="Here goes your To-Do!")
+
+labelTodosList = sg.Text(key="kLabelTodos")
+todosList = sg.Listbox(
+    key="kTodos", values=rw.readTodos(filePath), enable_events=True, size=(36, 12)
+)
 
 addButton = sg.Button(key="kAdd", button_text="Add")
 editButton = sg.Button(key="kEdit", button_text="Edit")
 completeButton = sg.Button(key="kComplete", button_text="Complete")
 exitButton = sg.Button(key="kExit", button_text="Exit Program")
 
-layout = [[today], [labelToDoText], [toDoText, addButton], [exitButton]]
+layout = [
+    [today],
+    [labelTodoText],
+    [todoText, addButton],
+    [labelTodosList],
+    [todosList, editButton, completeButton],
+    [exitButton],
+]
 
 
 def main():
     window = sg.Window(title=title, layout=layout, font=("Segoe UI", 12))
     while True:
         event, value = window.read()
-        print(event, value)
+        print(f"Event: {event}")
+        print(f"Todo List Value: {value["kTodos"]}")
+        print(f"All the Values: {value}")
 
         match event:
+            case "kTodos":
+                todo = value["kTodos"][0]
+                window["kTodo"].update(value=todo)
+
             case "kAdd":
                 todos = rw.readTodos(filePath)
 
-                todo = value["kToDo"] + "\n"
+                todo = tf.textBoxToTodo(value, "kTodo")
                 todos.append(todo)
 
                 rw.writeTodos(todos, filePath)
 
+                window["kTodos"].update(values=todos)
+
             case "kEdit":
-                print()
+                todos = rw.readTodos(filePath)
+
+                todo = tf.textBoxToTodo(value, "kTodo")
+                tempTodo = value["kTodos"][0]
+                index = todos.index(tempTodo)
+                todos[index] = todo
+
+                rw.writeTodos(todos)
+
+                window["kTodos"].update(values=todos)
 
             case "kComplete":
                 print()
