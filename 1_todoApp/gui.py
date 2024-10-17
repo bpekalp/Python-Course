91b9d773ee20bec871f4892d01db23cf478a6c1a
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-10-17 12:17:21 +0300

    Initial commit for gui application
    Experimenting with FreeSimpleGUI library.

---
+++ 1_todoApp/gui.py
@@ -0,0 +1,25 @@
+import readWrite as rw
+import stringToNum as stn
+import dateTimeFormatter as dtf
+import FreeSimpleGUI as gui
+
+title = "Coolest To-Do App Ever!"
+
+today = gui.Text(dtf.formatDateTime())
+
+labelTextBox = gui.Text("Enter a To-Do")
+textBox = gui.InputText(tooltip="Here goes your To-Do!")
+
+addButton = gui.Button("Add")
+
+layout = [[today], [labelTextBox], [textBox, addButton]]
+
+
+def main():
+    window = gui.Window(title=title, layout=layout)
+    window.read()
+    window.close()
+
+
+if __name__ == "__main__":
+    main()
