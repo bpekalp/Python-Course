6c82ac56b01c0ecd9ea6be91726046e2b1bf3202
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-10-19 19:35:33 +0300

    Add docstring to indexer function

--- 1_todoApp/func/indexer.py
+++ 1_todoApp/func/indexer.py
@@ -1,4 +1,14 @@
 def getTodoIndex(_values, _key, _todos):
-    tempTodo = _values[_key][0]
-    index = _todos.index(tempTodo)
+    """Gets the specified To-Do index and returns it.
+
+    Args:
+        _values (dict): The dictionary provided by window.read() function
+        _key (str): A key of the dictionary
+        _todos (list): To-Do list of the user
+
+    Returns:
+        int: Returns the index of a To-Do in To-Do list
+    """
+    todo = _values[_key][0]
+    index = _todos.index(todo)
     return index
