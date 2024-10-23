ed2874e8c6e28586913b1037d160bde6ddca3c0e
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-10-23 13:29:01 +0300

    Convert users webcam photo to grayscale

--- bonus/grayScaleWeb.py
+++ bonus/grayScaleWeb.py
@@ -1,1 +1,12 @@
 import streamlit as st
+from PIL import Image
+
+st.header("Grayscale Converter")
+st.subheader("Say Cheese!")
+cameraImage = st.camera_input("Take a photo", label_visibility="hidden")
+
+st.write("Here is your converted image:")
+if cameraImage:
+    grayScaleImage = Image.open(cameraImage)
+    grayScaleImage = grayScaleImage.convert("L")
+    st.image(grayScaleImage)
