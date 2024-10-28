28c47b4f89a82cbc53ce849234a54fbd33754286
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-10-28 13:24:53 +0300

    Display projects
    Even indexes will be displayed on the left and odd indexes will be displayed on the right column.

--- 2_ePortfolio/Home.py
+++ 2_ePortfolio/Home.py
@@ -1,13 +1,15 @@
 import streamlit as st
+import pandas as pd
+import func.projectDisplayer as fpd
 
 st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+left1, right1 = st.columns(2)
 
-with col1:
+with left1:
     st.image("2_ePortfolio/datasource/images/photo.png")
 
-with col2:
+with right1:
     content = """
     This is Ardit Sulce. The teacher of the Python Mega Course on Udemy.com
     Excepteur consectetur enim incididunt eiusmod minim ut Lorem. Non ea eu dolore elit velit excepteur. Veniam laborum id do incididunt proident laboris sit duis velit do reprehenderit. Elit non tempor id aute exercitation voluptate non proident nulla nostrud est eu duis. Pariatur duis labore incididunt in.
@@ -23,3 +25,16 @@ Below, you'll find all the projects Ardit made.
 Cillum nulla culpa velit nisi cupidatat consequat nisi incididunt exercitation consequat magna adipisicing culpa irure. Do est eiusmod ullamco qui pariatur exercitation. Duis consectetur occaecat dolore cillum officia esse reprehenderit ad veniam. Irure culpa enim et irure consequat.
 """
 st.write(content)
+
+projects = pd.read_csv("2_ePortfolio/datasource/data.csv", sep=";")
+left2, right2 = st.columns(2)
+
+with left2:
+    for i, project in projects.iterrows():
+        if i % 2 == 0:
+            fpd.displayProject(project)
+
+with right2:
+    for i, project in projects.iterrows():
+        if i % 2 != 0:
+            fpd.displayProject(project)
