6d2849beb7a94aad2bb4bd9c0157a513b81e27e9
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-10-28 12:54:36 +0300

    Add description outside the columns

--- 2_ePortfolio/Home.py
+++ 2_ePortfolio/Home.py
@@ -9,7 +9,17 @@ with col1:
 
 with col2:
     content = """
-    Laboris nisi esse anim id. Excepteur dolor duis incididunt reprehenderit tempor dolore amet in minim duis exercitation nostrud. Incididunt ad mollit quis pariatur eiusmod veniam exercitation enim ut amet sit eiusmod. Cupidatat quis deserunt sit officia consequat nulla in elit. Nisi enim et laborum ut esse adipisicing in dolor adipisicing ad consectetur in. Irure reprehenderit labore reprehenderit velit.
+    This is Ardit Sulce. The teacher of the Python Mega Course on Udemy.com
+    Excepteur consectetur enim incididunt eiusmod minim ut Lorem. Non ea eu dolore elit velit excepteur. Veniam laborum id do incididunt proident laboris sit duis velit do reprehenderit. Elit non tempor id aute exercitation voluptate non proident nulla nostrud est eu duis. Pariatur duis labore incididunt in.
+
+    Irure esse sit qui deserunt nulla eu. Aute ullamco non Lorem consectetur anim minim commodo aliquip id proident aliqua qui do nulla. Veniam officia commodo magna proident magna sit laboris id. Ullamco velit sit ad nisi.
     """
     st.header("Lorem Ipsum")
-    st.write(content)
+    st.info(content)
+
+content = """
+Below, you'll find all the projects Ardit made.
+
+Cillum nulla culpa velit nisi cupidatat consequat nisi incididunt exercitation consequat magna adipisicing culpa irure. Do est eiusmod ullamco qui pariatur exercitation. Duis consectetur occaecat dolore cillum officia esse reprehenderit ad veniam. Irure culpa enim et irure consequat.
+"""
+st.write(content)
