0afd68311192f67e5975cf895cfa89bd8239263d
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-10-29 22:33:12 +0300

    Implement sending e-mail functionality

--- 2_ePortfolio/pages/Contact_Me.py
+++ 2_ePortfolio/pages/Contact_Me.py
@@ -1,19 +1,38 @@
 import streamlit as st
+import func.emailSender as fes
 
 st.header("Contact Me!")
 with st.form(key="form_email"):
+    userName = st.text_input(
+        label="Your Name",
+        label_visibility="collapsed",
+        placeholder="Your Name...",
+        key="tb_userName",
+    )
     userMail = st.text_input(
         label="Your E-Mail...",
-        label_visibility="hidden",
+        label_visibility="collapsed",
         placeholder="Your E-Mail...",
         key="tb_userEmail",
     )
     userMessage = st.text_area(
         label="Your Message...",
-        label_visibility="hidden",
+        label_visibility="collapsed",
         placeholder="Your Message...",
         key="tb_userMessage",
     )
     submit = st.form_submit_button("Submit")
+
+if submit:
+    name = str(st.session_state["tb_userName"]).strip().title()
+    sender = st.session_state["tb_userEmail"]
+    message = st.session_state["tb_userMessage"]
+    status = fes.sendEmail(name, sender, message)
+
+    if status == "success":
+        st.success("Your e-mail was sent successfully!")
+
+    else:
+        st.error(status)
 
 st.session_state
