a2fa98863b7b6bc1796ba03ba5207f64a9062a2c
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-10-30 14:34:35 +0300

    Add page titles and generate pages according to topic data

--- 3_pdfMaker/main.py
+++ 3_pdfMaker/main.py
@@ -1,11 +1,24 @@
 from fpdf import FPDF
+import pandas as pd
+
+topics = pd.read_csv("3_pdfMaker/datasource/topics.csv")
 
 pdf = FPDF(orientation="portrait", unit="mm", format="A4")
 
 size = 12
-pdf.set_font(family="Times", size=size)
+lineX1 = 10
+lineX2 = 200
+lineY = 2.0 * size
 
-pdf.add_page()
-pdf.cell(w=0, h=size, txt="Hello World!", border=1, ln=1)
+for i, topic in topics.iterrows():
+    title = topic["Topic"]
+    pages = topic["Pages"]
+    pdf.add_page()
+    pdf.set_font(family="Times", size=2.0 * size)
+    pdf.cell(w=0, h=size, txt=title, border=0, ln=1)
+    pdf.line(lineX1, lineY, lineX2, lineY)
+
+    for j in range(int(pages)):
+        pdf.add_page()
 
 pdf.output("output.pdf")
