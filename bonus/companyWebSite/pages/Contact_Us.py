133f31b779a8c7f7907af44d6e52eb341a1bc2c8
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-10-30 12:48:00 +0300

    Revert "Add topic selection feature"
    This reverts commit b7f3d24999b4c775565a2616a96365be8ef7d29b.

--- bonus/companyWebSite/pages/Contact_Us.py
+++ bonus/companyWebSite/pages/Contact_Us.py
@@ -2,10 +2,9 @@ import streamlit as st
 import smtplib
 import ssl
 import json
-import pandas as pd
 
 
-def sendEmail(_name, _from, _topic, _message):
+def sendEmail(_name, _from, _message):
     host = "smtp.gmail.com"
     port = 465
     context = ssl.create_default_context()
@@ -21,7 +20,7 @@ def sendEmail(_name, _from, _topic, _message):
 From: {_name} <{_from}>
 Reply-To: {_from}
 To: {email}
-Subject: {_topic}, Sent by {_from}
+Subject: You Have a New E-mail From {_name}
 
 Keep in mind: The sender is {_from} but it will show up as {email}, you will be replying to the actual sender.
 
@@ -40,16 +39,9 @@ Keep in mind: The sender is {_from} but it will show up as {email}, you will be 
         return e
 
 
-topics = pd.read_csv("bonus/companyWebSite/datasource/topics.csv")
-
 with st.form(key="form_contactUs"):
     st.text_input(label="Your Name", key="tb_name")
     st.text_input(label="Your E-Mail", key="tb_email")
-    st.selectbox(
-        label="Select The Topic You Want To Discuss",
-        options=topics["topic"],
-        key="sb_topic",
-    )
     st.text_area(label="Your Message", key="tb_message")
     submit = st.form_submit_button("Submit")
 
@@ -57,14 +49,13 @@ with st.form(key="form_contactUs"):
 if submit:
     userName = str(st.session_state["tb_name"]).strip().title()
     userMail = st.session_state["tb_email"]
-    userTopic = st.session_state["sb_topic"]
     userMessage = st.session_state["tb_message"]
 
-    if not userName or not userMail or not userTopic or not userMessage:
+    if not userName or not userMail or not userMessage:
         st.error("Please fill the form before sending.")
 
     else:
-        status = sendEmail(userName, userMail, userTopic, userMessage)
+        status = sendEmail(userName, userMail, userMessage)
 
         if status == "success":
             st.success("Your e-mail was sent successfully!")
