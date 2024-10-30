941733a6f6019606307d1e2093056a32fec3e159
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-10-30 14:43:47 +0300

    Generate 1 page less
    Total page amount should be equal to the "Pages" in topics.csv

--- 3_pdfMaker/main.py
+++ 3_pdfMaker/main.py
@@ -6,6 +6,7 @@ topics = pd.read_csv("3_pdfMaker/datasource/topics.csv")
 pdf = FPDF(orientation="portrait", unit="mm", format="A4")
 
 size = 12
+
 lineX1 = 10
 lineX2 = 200
 lineY = 2.0 * size
@@ -13,12 +14,15 @@ lineY = 2.0 * size
 for i, topic in topics.iterrows():
     title = topic["Topic"]
     pages = topic["Pages"]
+
     pdf.add_page()
+
     pdf.set_font(family="Times", size=2.0 * size)
     pdf.cell(w=0, h=size, txt=title, border=0, ln=1)
+
     pdf.line(lineX1, lineY, lineX2, lineY)
 
-    for j in range(int(pages)):
+    for j in range(int(pages) - 1):
         pdf.add_page()
 
 pdf.output("output.pdf")
