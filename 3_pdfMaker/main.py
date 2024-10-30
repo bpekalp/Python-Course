c21d547b6bb651e448c435a6f08456fd41e4d46f
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-10-30 13:12:37 +0300

    PDF hello world with set font family and size

--- 3_pdfMaker/main.py
+++ 3_pdfMaker/main.py
@@ -2,5 +2,10 @@ from fpdf import FPDF
 
 pdf = FPDF(orientation="portrait", unit="mm", format="A4")
 
+size = 12
+pdf.set_font(family="Times", size=size)
+
 pdf.add_page()
+pdf.cell(w=0, h=size, txt="Hello World!", border=1, ln=1)
+
 pdf.output("output.pdf")
