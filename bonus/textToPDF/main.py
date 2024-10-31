87b1f8c3fc7466b086efa3dcd272ecfd79774d84
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-10-31 16:47:42 +0300

    Generate PDF files from .txt file contents

---
+++ bonus/textToPDF/main.py
@@ -0,0 +1,29 @@
+from fpdf import FPDF
+import glob
+import os
+
+dataSource = "bonus/textToPDF/datasource"
+fileNames = glob.glob(f"{dataSource}/*.txt")
+
+pdf = FPDF(orientation="p", unit="mm", format="a4")
+
+for fileName in fileNames:
+    with open(fileName, "r") as file:
+        content = file.read()
+
+    title = str(os.path.splitext(os.path.basename(fileName))[0]).title()
+
+    pdf.add_page()
+
+    size = 16
+    pdf.set_font(family="Times", style="b", size=size)
+    pdf.cell(w=0, h=size, txt=title, ln=1)
+
+    size = 12
+    pdf.set_font(family="Times", size=size)
+    pdf.multi_cell(w=0, h=size/2.0, txt=content)
+
+if not os.path.exists("PDFs/"):
+    os.makedirs("PDFs/")
+
+pdf.output(f"PDFs/textTo.pdf")
