906d2b3235fc039274bdf3d049ba4f53adfbd605
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-11-05 15:40:29 +0300

    Return dummy dictionary on API request

--- 6_weatherAPI/main.py
+++ 6_weatherAPI/main.py
@@ -10,4 +10,13 @@ def home():
     return flask.render_template("home.html")
 
 
+@app.route("/api/<station>/<date>")
+def api(station, date):
+    return {
+        "station": station,
+        "date": date,
+        "temperature": 40
+    }
+
+
 app.run(debug=True)
