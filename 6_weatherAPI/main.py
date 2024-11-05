472e30652cb5b4d09442f40660b9746ad6b9c7a0
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-11-05 15:14:41 +0300

    Display cat.png

--- 6_weatherAPI/main.py
+++ 6_weatherAPI/main.py
@@ -1,6 +1,8 @@
 import flask
 
-app = flask.Flask("flaskApp", template_folder="6_weatherAPI/templates")
+app = flask.Flask("flaskApp",
+                  template_folder="6_weatherAPI/templates",
+                  static_folder="6_weatherAPI/static")
 
 
 @app.route("/")
