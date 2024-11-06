d24a7193a4de984bc0b8e37d0185a4693fd98b63
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-11-06 16:06:56 +0300

    Create Weather API backend

--- 6_weatherAPI/main.py
+++ 6_weatherAPI/main.py
@@ -6,12 +6,42 @@ app = flask.Flask(__name__, template_folder="templates", static_folder="static")
 
 @app.route("/")
 def home():
-    return flask.render_template("home.html")
+    df = pd.read_csv("6_weatherAPI/weatherData/stations.txt", skiprows=17)
+    df.columns = df.columns.str.strip()
+    df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+    stations = df[["STAID", "STANAME"]].to_dict(orient="records")
+    return flask.render_template("home.html", stations=stations)
+
+
+def getStationId(_station):
+    df = pd.read_csv("6_weatherAPI/weatherData/stations.txt", skiprows=17)
+    df.columns = df.columns.str.strip()
+    df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+
+    station = df.loc[df["STANAME"] == str(_station).upper()]["STAID"].squeeze()
+
+    return station
+
+
+def getTemperature(_station, _date):
+    station = getStationId(_station)
+
+    fileToRead = "TG_STAID" + str(station).zfill(6) + ".txt"
+
+    df = pd.read_csv(f"6_weatherAPI/weatherData/{fileToRead}", skiprows=20)
+    df.columns = df.columns.str.strip()
+    df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+
+    temperature = df.loc[df["DATE"] == int(_date)]["TG"].squeeze() / 10.0
+
+    return temperature
 
 
 @app.route("/api/<station>/<date>")
 def api(station, date):
-    return {"station": station, "date": date, "temperature": 14}
+
+    temperature = getTemperature(station, date)
+    return {"station": station, "date": date, "temperature": temperature}
 
 
 if __name__ == "__main__":
