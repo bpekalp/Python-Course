58ec209280dfe275fd8feeb895f58e23dbec4d11
Güven Barış PEKALP <b.pekalp@gmail.com>
2024-11-11 13:04:01 +0300

    Update variable names

--- 7_weatherForecast/Home.py
+++ 7_weatherForecast/Home.py
@@ -3,27 +3,23 @@ import plotly.express as px
 
 st.header("Best Weather Forecast App!")
 
-st.text_input("Location", key="tb_Location")
-st.slider("Forecast Days", min_value=1, max_value=5, step=1, key="sl_Days")
-st.selectbox(
-    "Select Weather Data To View", options=("Temperature", "Sky"), key="dl_SubData"
+location = st.text_input("Location", key="tb_Location")
+day = st.slider("Forecast Days", min_value=1, max_value=5, step=1, key="sl_Days")
+weatherOpt = st.selectbox(
+    "Select Weather Data To View", options=("Temperature", "Sky"), key="dl_WeatherOpt"
 )
 
-location = st.session_state["tb_Location"]
-day = st.session_state["sl_Days"]
-subData = st.session_state["dl_SubData"]
-
-if location and day and subData:
-    message = f"{subData} For Next {day} Days In {location}"
+if location and day and weatherOpt:
+    message = f"{weatherOpt} For Next {day} Days In {location}"
     st.subheader(message)
 
     dates = ("2022-25-10", "2022-26-10", "2022-27-10")
     temps = (14, 27, 34)
-    if subData == "Temperature":
+    if weatherOpt == "Temperature":
         plot = px.line(x=dates, y=temps, labels={"x": "Date", "y": "Temperature (C)"})
         st.plotly_chart(plot)
 
-    elif subData == "Sky":
+    elif weatherOpt == "Sky":
         pass
 
 
